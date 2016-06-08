8c5e3b0a9cae9e70cce95cf769f94261ab0777f3
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-08 17:51:13 -0400

    initial commit

---
+++ DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  SQLite3DBSample
+//
+//  Created by Gabriel Theodoropoulos on 25/6/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
---
+++ DBManager.m
@@ -0,0 +1,13 @@
+//
+//  DBManager.m
+//  SQLite3DBSample
+//
+//  Created by Gabriel Theodoropoulos on 25/6/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import "DBManager.h"
+
+@implementation DBManager
+
+@end
